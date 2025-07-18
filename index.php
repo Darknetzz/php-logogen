76e8dcf6cfb78975d51b42121b1b22b646c36be8
Kristian <github@roste.org>
2025-07-18 12:22:14 +0200

    update tabler to 1.4.0

--- index.php
+++ index.php
@@ -15,10 +15,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- <link href="css/tabler.min.css" rel="stylesheet"> -->
     <script src="js/jquery.min.js"></script>
-    <link rel="stylesheet"
-        href="https://cdn.jsdelivr.net/npm/@tabler/core@1.3.2/dist/css/tabler.min.css" />
     <script
-        src="https://cdn.jsdelivr.net/npm/@tabler/core@1.3.2/dist/js/tabler.min.js">
+    src="https://cdn.jsdelivr.net/npm/@tabler/core@1.4.0/dist/js/tabler.min.js">
+    </script>
+    <link rel="stylesheet"
+    href="https://cdn.jsdelivr.net/npm/@tabler/core@1.4.0/dist/css/tabler.min.css" />
     </script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
     <title>Image Generator</title>
@@ -56,8 +57,8 @@
                             <?= $shapes_select ?>
                         </td>
                     </tr>
-                    
-                    
+
+
                     <tr>
                         <th <?= $classes['input-title'] ?>>
                             Output
@@ -129,7 +130,7 @@
                     <tr>
                         <th <?= $classes['table-title'] ?>>
                             <!-- <label for="enablebordercheckbox" class="form-check form-switch form-switch-3"> -->
-                                Border 
+                                Border
                                 <!-- <input class="form-check-input toggleInput" type="checkbox" name="enableborder" id="enablebordercheckbox" data-target=".border-inputs"> -->
                             <!-- </label> -->
                         </th>
@@ -227,7 +228,7 @@
                     <tr>
                         <th <?= $classes['table-title'] ?>>
                             <label for="enablebordercheckbox" class="form-check form-switch form-switch-3">
-                                Filters 
+                                Filters
                                 <input class="form-check-input toggleInput" type="checkbox" name="enablefilters" id="enablefilterscheckbox" data-target=".filters-inputs">
                             </label>
                         </th>
@@ -251,7 +252,7 @@
                 <thead <?= $classes['thead'] ?>>
                     <th <?= $classes['table-title'] ?>>
                         <label for="enabledebugcheckbox" class="form-check form-switch form-switch-3">
-                            Debug 
+                            Debug
                             <input class="form-check-input toggleInput" type="checkbox" name="enabledebug" id="enabledebugcheckbox" data-target=".debug">
                         </label>
                     </th></tr>
